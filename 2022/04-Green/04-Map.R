5849b8f033edd5011f4a699b6475ab805b55d35a
Jean Dupin <jean.dupin69@gmail.com>
2022-11-04 09:54:13 +0100

    04 SVG

--- 2022/04-Green/04-Map.R
+++ 2022/04-Green/04-Map.R
@@ -3,6 +3,10 @@
 library(tidyverse)
 library(sf)
 
+scale_fill_fermenter_custom <- function(pal, na.value = "#C6C6C6", guide = "coloursteps", aesthetics = "fill", ...) {
+  binned_scale("fill", "fermenter", ggplot2:::binned_pal(scales::manual_pal(unname(pal))), na.value = na.value, guide = guide, ...)  
+}
+
 
 # Donnees ----
 
@@ -48,11 +52,35 @@ world <-
 
 # Carte ----
 
-# camcorder::gg_record(width = 21, height = 21,
-#                      units = "cm")
+camcorder::gg_record(width = 21, height = 21,
+                     units = "cm")
 
 
 ggplot(world) +
-  geom_sf(aes(fill = part_vert)) +
-  coord_sf(xlim = c(st_bbox(africa)[1],st_bbox(africa)[3]),
-           ylim = c(st_bbox(africa)[2],st_bbox(africa)[4]))
+  geom_sf(aes(fill = part_vert),
+          size = .1,
+          color = "#646363") +
+  scale_fill_fermenter_custom(CGI::insee.pal("Vert",0,5),
+                              breaks = seq(0,100,20)) +
+  theme_void() +
+  theme(legend.direction = "horizontal",
+        legend.text = element_blank(),
+        legend.position = c(.2,.2),
+        legend.title.align = .5,
+        legend.key.width = unit(1.3,"cm"),
+        legend.key.height = unit(.4,"cm"),
+        plot.background = element_rect(fill = "#8DB0E1",
+                                       color = "#8DB0E1"),
+        panel.background = element_rect(fill = "#8DB0E1",
+                                        color = "#8DB0E1")) +
+  coord_sf(st_bbox(africa)[c(1,3)],
+           st_bbox(africa)[c(2,4)]) +
+  labs(fill = "")
+
+
+ggsave(here::here("2022",
+                  "04-Green",
+                  "carte.png"),
+       width = 21,
+       height = 21,
+       units = "cm")
